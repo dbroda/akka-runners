d4adf531897f0ac9f9e0608dd4aed6bb9f68ad51
dbroda <dbroda@agora.pl>
2020-04-11 13:05:27 +0200

    Added gradle setup Added spring boot setup

---
+++ src/main/java/codeplus/kata/springakkarunner/SpringAkkaRunnerApplication.java
@@ -0,0 +1,13 @@
+package codeplus.kata.springakkarunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAkkaRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAkkaRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeplus/kata/springakkarunner/SpringAkkaRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package codeplus.kata.springakkarunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAkkaRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
